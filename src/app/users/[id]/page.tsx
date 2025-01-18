d2aa0bfcf5d192a952ad682236295c3e9c191d70
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-01-18 12:27:42 +0100

    add await to params

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -2,21 +2,23 @@ import UserDetails from '@/components/users/UserDetails/UserDetails';
 import { Metadata } from 'next';
 
 export interface PageProps {
-	params: { id: string };
+	params: Promise<{ id: string }>;
 }
 
 export const generateMetadata = async ({
 	params,
 }: PageProps): Promise<Metadata> => {
+	const resolvedParams = await params;
 	return {
-		title: `User ${params?.id}`,
-		description: `This is the profile page for user with ID ${params.id}`,
+		title: `User ${resolvedParams?.id}`,
+		description: `This is the profile page for user with ID ${resolvedParams.id}`,
 	};
 };
 export default async function Page({ params }: PageProps) {
-	if (!params?.id) {
+	const resolvedParams = await params;
+	if (!resolvedParams?.id) {
 		return <p>Error: Missing user ID</p>;
 	}
 
-	return <UserDetails id={params.id} />;
+	return <UserDetails id={resolvedParams?.id} />;
 }
