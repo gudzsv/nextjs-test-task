520a09074a96ae7f019c7f6303ccc7c43a3c49d8
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-01-18 12:21:14 +0100

    once more add sync to function page

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -5,15 +5,16 @@ export interface PageProps {
 	params: { id: string };
 }
 
-export const generateMetadata = ({ params }: PageProps): Metadata => {
+export const generateMetadata = async ({
+	params,
+}: PageProps): Promise<Metadata> => {
 	return {
-		title: `User ${params.id}`,
+		title: `User ${params?.id}`,
 		description: `This is the profile page for user with ID ${params.id}`,
 	};
 };
-
-export default function Page({ params }: PageProps) {
-	if (!params.id) {
+export default async function Page({ params }: PageProps) {
+	if (!params?.id) {
 		return <p>Error: Missing user ID</p>;
 	}
 
